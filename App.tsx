50d1ac72ba98e49288baab620955ec819c13039c
Jaehun <qkdwogns98@gmail.com>
2025-03-31 22:57:06 +0900

    Project init

---
+++ App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return <View>{/* ... */}</View>;
+}
+
+const styles = StyleSheet.create({
+  // ...
+});
+
+export default App;
