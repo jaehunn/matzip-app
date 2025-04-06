006a65fc7a9a05332380181db683df6f9caf173f
Jaehun <qkdwogns98@gmail.com>
2025-04-06 17:19:39 +0900

    chore: react-native-gesture-handler, conditionally import

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+/** @see {@react-navigation/stack} https://reactnavigation.org/docs/stack-navigator#installation */
+import 'react-native-gesture-handler';
+
 AppRegistry.registerComponent(appName, () => App);
